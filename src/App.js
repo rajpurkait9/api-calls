fbbb19992df85086d8e0b558b45ddd6c797662ba
Raj Purkait <rajpurkait9@gmail.com>
2022-04-09 17:01:41 +0530

    make all api calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  // console.log(data);
+  const [update, setUpdate] = useState(data);
+  const handleUpdate = async post => {
+    post.title = "UPDATED";
+    const newData = await axios.put(url + "/" + post.id, post);
+    setUpdate(newData, [...data]);
+  };
+  const handleClick = async () => {
+    const obj = { title: " raj purkait" };
+    const create = await axios.post(url, obj);
+    setData([create.data, ...data]);
+  };
+  const handleDelete = async post => {
+    await axios.delete(url + "/" + post.id);
+    const newData = data.filter(nd => nd.id !== post.id);
+    setData(newData);
+  };
+
+  useEffect(async () => {
+    const res = await axios.get(url);
+
+    setData(res.data);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className='btn btn-primary m-2' onClick={() => handleClick()}>
+        Add
+      </button>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>update btn</th>
+            <th>delete btn</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(singleData => {
+            return (
+              <tr key={singleData.id}>
+                <td>{singleData.title}</td>
+                <td>
+                  <button
+                    className='btn btn-warning m-2'
+                    onClick={() => handleUpdate(singleData)}
+                  >
+                    update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className='btn btn-danger m-2'
+                    onClick={() => handleDelete(singleData)}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
-}
+};
 
 export default App;
